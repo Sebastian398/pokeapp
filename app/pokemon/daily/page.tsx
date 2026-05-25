ad0f6d542c63ee8923d5559ca4b3da28cfccea74
Sebastian398 <neider.ramirezdelgadillo@gmail.com>
2026-05-24 23:46:01 -0500

    se ajusta el pokemon del dia

--- app/pokemon/daily/page.tsx
+++ app/pokemon/daily/page.tsx
@@ -18,8 +18,8 @@ export default async function DailyPokemonPage() {
   const pokemon = await fetchPokemon(data.id);
 
   return (
-    <div className="min-h-screen bg-gradient-to-b from-blue-400 to-blue-600 flex items-center justify-center">
-      <div className="bg-white rounded-lg shadow-lg p-8 text-center max-w-sm">
+    <div className="min-h-screen bg-gradient-to-b from-blue-500 to-blue-00 flex items-center justify-center">
+      <div className="bg-white rounded-2xl shadow-2xl p-10 text-center max-w-md border-4 border-green-300">
         <h1 className="text-3xl font-bold text-blue-600 mb-2">
           Pokémon del día 🎉
         </h1>
@@ -28,7 +28,7 @@ export default async function DailyPokemonPage() {
         <img
           src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
           alt={pokemon.name}
-          className="w-32 h-32 mx-auto mb-4"
+          className="w-35 h-35 mx-auto mb-4"
         />
 
         <p className="text-xl font-bold capitalize text-black">
